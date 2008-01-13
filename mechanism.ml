94b9cf42ac8e0d2ae663dfa88f0e04899cda523f
Berke Durak <berke@aurochs.fr>
2008-01-13 13:12:15 +0100

    Hashing stories.

--- mechanism.ml
+++ mechanism.ml
@@ -1,6 +1,7 @@
 (* Mechanism *)
 
 open Pffpsf
+open Reddit
 
 module Time =
   struct
@@ -40,9 +41,10 @@ type story = {
   mutable st_created     : Time.t;
   mutable st_last_event  : Time.t;
   mutable st_predicted   : Time.t; (* When next event is predicted *)
-  mutable st_up_votes    : int;
-  mutable st_down_votes  : int;
-  mutable st_comments    : int;
+  mutable st_up_votes    : int option;
+  mutable st_down_votes  : int option;
+  mutable st_comments    : int option;
+  mutable st_score       : int option;
 }
 
 type t = {
@@ -51,6 +53,13 @@ type t = {
   tk_exec     : (adder -> unit);
 }
 and adder = (t -> unit)
+
+type 'a context = {
+  cx_pipe    : 'a;
+  cx_url     : string;
+  cx_stories : (string, story) Hashtbl.t;
+  cx_add     : adder
+}
 
 module TQ = Prioqueue.Make(Time)
 
@@ -73,28 +82,76 @@ let scheduler initial =
     tk.tk_exec add
   done
 
-let perform url =
-  let pipe = new Www.HC.pipeline in
-  let with_doc url f =
-    let doc = Www.obtain_document pipe (Www.Get url) in
-    f doc
-  in
+let with_doc ~cx ~url f =
+  let doc = Www.obtain_document cx.cx_pipe (Www.Get url) in
+  f doc
 
-  let rec scan_task schedule =
+let scan_front cx =
+  pf "Scanning %S\n%!" cx.cx_url;
+  with_doc
+    ~cx
+    ~url:cx.cx_url
+    (fun doc -> process_front doc)
+
+let process_entry cx en =
+  match en.en_id with
+  | None -> pf "Story with no ID!\n%!"
+  | Some id ->
+      if not (Hashtbl.mem cx.cx_stories id) then
+        begin
+          pf "New story %S\n%!" id;
+          let t = Time.now () in
+          let st =
+            {
+              st_reddit      = cx.cx_url;
+              st_title       = en.en_title;
+              st_url         = en.en_url;
+              st_submitter   = en.en_user;
+              st_id          = id;
+              st_created     = t;
+              st_last_event  = t;
+              st_predicted   = t +. !Opt.delay;
+              st_up_votes    = None;
+              st_down_votes  = None;
+              st_comments    = en.en_comments;
+              st_score       = en.en_score;
+            }
+          in
+          Hashtbl.add cx.cx_stories id st
+    end
+
+let perform url =
+  let scan_delay = 10.0 in
+
+  let rec scan_task cx schedule =
     {
       tk_name     = "Scan";
       tk_schedule = schedule;
       tk_exec     =
         (fun add ->
           pf "Executing scan\n%!";
-          with_doc
-            url
-            (fun doc ->
-              let entries = Reddit.process_front doc in
-              List.iter (Reddit.print_entry stdout) entries;
-              add (scan_task (Time.now () +. 10.0))
-            )
+          let entries = scan_front cx in
+          List.iter (process_entry cx) entries;
+          add (scan_task cx (Time.now () +. scan_delay))
         )
     }
   in
-  scheduler (scan_task 0.0);
+
+  let initial add =
+    let cx =
+      {
+        cx_pipe    = new Www.HC.pipeline;
+        cx_stories = Hashtbl.create 1000;
+        cx_add     = add;
+        cx_url     = url;
+      }
+    in
+    add (scan_task cx 0.0)
+  in
+
+  scheduler
+    {
+      tk_name     = "Init";
+      tk_schedule = 0.0;
+      tk_exec     = initial
+    }
--- opt.ml
+++ opt.ml
@@ -1,6 +1,6 @@
 (* Opt *)
 
-let delay = ref 60.0;;
+let delay = ref 10.0;;
 let config_file = ref "monitor.config";;
 let debug = ref false;;
 let sendmail = ref "/usr/sbin/sendmail ${WHO}";;
